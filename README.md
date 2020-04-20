55b7485b0bcbc91743c30f3257b3c4bc176a26c7
derekdkim <ddkim1024@gmail.com>
2020-04-20 15:39:27 -0500

    First commit. Backend implemented

---
+++ README.md
@@ -0,0 +1,7 @@
+# Pomodoro Clock
+
+A basic Pomodoro Clock made using vanilla JS.
+
+## Progress:
+- Working business logic.
+- Front-end needs to be designed and implemented.
---
+++ script.js
@@ -0,0 +1,160 @@
+class Timer {
+  constructor () {
+    this.isActive = false;
+    this.isComplete = false;
+    this.startTime = 0;
+    this.totalTimeElapsed = 0;
+    this.goalTime = 1000 * 60 * 25;
+  }
+
+  raiseGoalTime () {
+    if (this.goalTime < 1000 * 60 * 90) {
+      this.goalTime = this.goalTime + (1000 * 60);
+    }
+  }
+
+  lowerGoalTime () {
+    if (this.goalTime > 1000 * 60) {
+      this.goalTime = this.goalTime - (1000 * 60);
+    }
+  }
+
+  timeElapsed () {
+    return Date.now() - this.startTime;
+  }
+
+  getTime () {
+    if (this.isActive) {
+      return this.goalTime - (this.totalTimeElapsed + this.timeElapsed());
+    }
+    return this.goalTime - this.totalTimeElapsed;
+  }
+
+  start () {
+    if (!this.isActive) {
+      console.log('Started!');
+      this.isActive = true;
+      this.startTime = Date.now();
+    }
+  }
+
+  pause () {
+    if (this.isActive) {
+      console.log('Paused!');
+      this.isActive = false;
+      this.totalTimeElapsed += this.timeElapsed();
+    }
+  }
+
+  reset () {
+    console.log('Stopped!');
+    this.isActive = false;
+    this.isComplete = false;
+    this.startTime = 0;
+    this.totalTimeElapsed = 0;
+  }
+}
+
+// Time formatter
+const formatSecToMMSS = (sec) => {
+  const min = Math.floor(sec / 60);
+  const remSec = sec % 60;
+
+  return twoDigits(min) + ':' + twoDigits(remSec);
+};
+
+const twoDigits = (num) => {
+  if (num < 10) {
+    return '0' + num.toString();
+  } else {
+    return num.toString();
+  }
+};
+
+// Main function
+const pomodoro = new Timer();
+const breakTimer = new Timer();
+pomodoro.goalTime = 1000 * 60 * 25;
+breakTimer.goalTime = 1000 * 60 * 5;
+
+document.addEventListener('DOMContentLoaded', () => {
+  intervalTimer();
+});
+
+// Display current time parameters
+const intervalTimer = () => {
+  document.getElementById('pomodoro-time').textContent = pomodoro.goalTime / (1000 * 60);
+  document.getElementById('break-time').textContent = breakTimer.goalTime / (1000 * 60);
+
+  const interval = setInterval(() => {
+    const timeInSec = Math.round(pomodoro.getTime() / 1000);
+    if (timeInSec > 0) {
+      document.getElementById('time-display').textContent = formatSecToMMSS(timeInSec);
+    }
+    if (timeInSec === 0 && !pomodoro.isComplete) {
+      pomodoro.isComplete = true;
+      breakTimer.start();
+      pomodoro.pause();
+    }
+
+    // Start Break Timer
+    if (pomodoro.isComplete) {
+      const breakTimeInSec = Math.round(breakTimer.getTime() / 1000);
+      document.getElementById('time-display').textContent = formatSecToMMSS(breakTimeInSec);
+
+      if (breakTimeInSec === 0) {
+        breakTimer.reset();
+        clearInterval(interval);
+      }
+    }
+  }, 100);
+};
+
+// Start button
+document.getElementById('start').addEventListener('click', () => {
+  if (!pomodoro.isComplete) {
+    pomodoro.start();
+  } else {
+    breakTimer.start();
+  }
+});
+
+// Pause button
+document.getElementById('pause').addEventListener('click', () => {
+  if (!pomodoro.isComplete) {
+    pomodoro.pause();
+  } else {
+    breakTimer.pause();
+  }
+});
+
+// Reset button
+document.getElementById('reset').addEventListener('click', () => {
+  pomodoro.reset();
+  breakTimer.reset();
+  pomodoro.goalTime = 1000 * 60 * 25;
+  breakTimer.goalTime = 1000 * 60 * 5;
+  intervalTimer();
+});
+
+// Pomodoro Time Toggle Buttons
+document.getElementById('lower-pomo-time').addEventListener('click', () => {
+  pomodoro.lowerGoalTime();
+  document.getElementById('pomodoro-time').textContent = pomodoro.goalTime / (1000 * 60);
+});
+
+document.getElementById('raise-pomo-time').addEventListener('click', () => {
+  pomodoro.raiseGoalTime();
+  document.getElementById('pomodoro-time').textContent = pomodoro.goalTime / (1000 * 60);
+});
+
+// Break Time Toggle Buttons
+document.getElementById('lower-break-time').addEventListener('click', () => {
+  breakTimer.lowerGoalTime();
+  document.getElementById('break-time').textContent = breakTimer.goalTime / (1000 * 60);
+});
+
+document.getElementById('raise-break-time').addEventListener('click', () => {
+  breakTimer.raiseGoalTime();
+  document.getElementById('break-time').textContent = breakTimer.goalTime / (1000 * 60);
+});
